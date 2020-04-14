0e3f2620b870c0ae76b5525dbb05e86c4e4e7965
Dzianis Pasanen <pasanenD@yandex.by>
2020-04-14 08:22:44 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,16 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var s="YES"
+        val newStr = StringBuilder(a)
+        val newStr2 = StringBuilder(b)
+        for (i2 in newStr2.indices){
+            if (!newStr.toString().substring(i2).toLowerCase().contains(newStr2[i2].toLowerCase())){
+                s="NO"
+                break
+            }
+        }
+        return s
     }
 }
+
