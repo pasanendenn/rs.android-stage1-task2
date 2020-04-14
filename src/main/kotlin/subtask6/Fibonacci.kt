1bbc664550a52f1ef2db3248dc2a8006150e9971
Dzianis Pasanen <pasanenD@yandex.by>
2020-04-14 08:26:20 +0300

    Update Fibonacci.kt

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,23 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val arr = arrayListOf(0,1)
+        var a = 0
+        var b = 0
+        while (a<n){
+            a=arr[b]*arr[b+1]
+            arr.add(arr[b]+arr[b+1])
+            b+=1
+        }
+
+        val newArray1 = intArrayOf(arr[b-1], arr[b], 1)
+        val newArray2 = intArrayOf(arr[b-1], arr[b], 0)
+
+        return if (a==n){
+            newArray1
+        } else {
+            newArray2
+        }
+
     }
 }
